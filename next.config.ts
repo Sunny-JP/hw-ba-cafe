5fccf4e63f6b03d6ecf37634096a1ae76e4ff195
Sunny <kajimalu10@gmail.com>
2025-12-28 00:57:04 +0900

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
+// 開発環境か本番環境かを判定する
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
+    unoptimized: true,
   },
+
+  // 本番ビルド(npm run build)の時だけ 'export' にする
+  // 開発中(npm run dev)は undefined (通常モード) で動かす
+  output: isProd ? 'export' : undefined,
 };
 
 export default nextConfig;
