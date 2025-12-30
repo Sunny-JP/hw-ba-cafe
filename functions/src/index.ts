222bbc6f17ebc28a2b416cdb6c66e9269b5caf40
Sunny <kajimalu10@gmail.com>
2025-12-30 22:36:28 +0900

    debug

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,8 +20,9 @@ export const scheduleNotification = functions.region(location).https.onCall(asyn
   }
 
   const userId = context.auth.uid;
-  const delayHours = 3; 
-  const seconds = delayHours * 60 * 60; 
+  // const delayHours = 3; 
+  // const seconds = delayHours * 60 * 60; 
+  const seconds = 900; // デバッグ用: 15分後に設定
 
   // Cloud Tasksが実行する関数のURL
   const url = `https://${location}-${project}.cloudfunctions.net/sendFcmNotification`;
