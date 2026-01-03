52012149b10357da406dda4824f1d851959fc4cb
Sunny <kajimalu10@gmail.com>
2026-01-03 14:35:46 +0900

    adjust style

--- src/app/operator/page.tsx
+++ src/app/operator/page.tsx
@@ -8,26 +8,20 @@ export default function OperatorPage() {
         
         <div className="space-y-4 text-muted-foreground">
           <div className="flex flex-col gap-2">
-            <h3 className="font-bold text-foreground">開発・運営</h3>
-            <p>さにー (@156miyako)</p>
+            <h3 className="font-bold text-foreground">開発</h3>
+            <p className="font-normal">
+              代表/運営: さにー (<a href="https://x.com/156miyako" target="_blank" rel="noopener noreferrer" className="hover:underline text-blue-400">@156miyako</a>)<br />
+              ＊＊＊ (<a href="https://x.com/" target="_blank" rel="noopener noreferrer" className="hover:underline text-blue-400">@******</a>)</p>
           </div>
 
           <div className="flex flex-col gap-2 mt-4">
             <h3 className="font-bold text-foreground">連絡先</h3>
-            <p>不具合報告やご要望は、以下のリンク（GitHubのissue/PR または X/Twitter）までお願いいたします。</p>
-            <p>なお、個人開発ゆえに全ての不具合やご要望に対応できない場合があります。ご了承ください。</p>
-            <ul className="list-disc pl-5 text-blue-400">
-              <li>
-                <a href="https://github.com/Sunny-JP/hw-ba-cafe" target="_blank" rel="noopener noreferrer" className="hover:underline">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <a href="https://x.com/156miyako" target="_blank" rel="noopener noreferrer" className="hover:underline">
-                  X (Twitter)
-                </a>
-              </li>
-            </ul>
+            <p className="font-normal">不具合報告やご要望は、 
+              <a href="https://github.com/Sunny-JP/hw-ba-cafe" target="_blank" rel="noopener noreferrer" className="hover:underline text-blue-400">
+                GitHubリポジトリ
+              </a> のissue または X (Twitter) までお願いいたします。<br />
+            なお、個人開発のため全ての不具合やご要望に対応できない場合があります。ご了承ください。<br />
+            共同開発も受け付けています。GitHubリポジトリでPRしていただければ、採用されるかもしれません。バグ修正や機能改善など、積極的にご参加いただけるとありがたいです。</p>
           </div>
         </div>
 
