67c45abfe874393f5272d17029c39fa7d2ae9619
Sunny <kajimalu10@gmail.com>
2026-01-30 02:14:32 +0900

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,6 +6,17 @@ import { useEffect } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+  const Message = `
+    ____        __      _____ _                         
+   / ___|__ _  / _| ___|_   _(_)_ __ ___   ___ _ __ 
+  | |   / _\` || |_ / _ \\ | | | | '_ \` _ \\ / _ \\ '__|
+  | |__| (_| ||  _|  __/ | | | | | | | | |  __/ |   
+   \\____\\__,_||_|  \\___| |_| |_|_| |_| |_|\\___|_|   
+
+  >> Welcome, Sensei.
+  >> I've been waiting for your next order.`;
+    console.log("%c" + Message, "color: #7ad3ff; font-weight: bold; font-family: monospace;");
+    
     const setSvh = () => {
       document.documentElement.style.setProperty('--svh', `${window.innerHeight}px`);
     };
