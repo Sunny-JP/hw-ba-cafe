272b60d857c11c3f1de7331aece5a171f5a8d2fd
Sunny <kajimalu10@gmail.com>
2026-02-01 01:18:14 +0900

    update sec

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,17 @@ import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.onesignal.com https://onesignal.com https://static.cloudflareinsights.com;
+default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.onesignal.com https://onesignal.com https://static.cloudflareinsights.com https://www.googletagmanager.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://lh3.googleusercontent.com https://cdn.discordapp.com https://*.onesignal.com;
+    img-src 'self' blob: data: https://lh3.googleusercontent.com https://cdn.discordapp.com https://*.onesignal.com https://www.google-analytics.com https://www.googletagmanager.com;
     font-src 'self';
-    connect-src 'self' https://*.supabase.co wss://*.supabase.co https://*.onesignal.com https://onesignal.com https://cloudflareinsights.com;
+    connect-src 'self' https://*.supabase.co wss://*.supabase.co https://*.onesignal.com https://onesignal.com https://cloudflareinsights.com https://www.google-analytics.com;
     object-src 'none';
     base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
     upgrade-insecure-requests;
+    frame-ancestors 'none';
+    form-action 'self';
 `.replace(/\s{2,}/g, " ").trim();
 
 const nextConfig: NextConfig = {
@@ -48,7 +48,7 @@ const nextConfig: NextConfig = {
         ],
       },
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           { key: 'Content-Security-Policy', value: cspHeader },
           { key: 'X-Frame-Options', value: 'DENY' },
