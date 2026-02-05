dc7f2c407009e2b53e78f4ed6d9fd4a65035739f
Sunny <kajimalu10@gmail.com>
2026-02-05 13:11:32 +0900

    cursor pointer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header({ onMenuClick, isLoggedIn = false }: HeaderProps)
         {isLoggedIn && (
           <button 
             onClick={onMenuClick}
-            className="p-2 rounded-full"
+            className="p-2 rounded-full cursor-pointer"
             aria-label="メニューを開く"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
